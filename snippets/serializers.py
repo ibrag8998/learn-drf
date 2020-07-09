fe82e1e1573916f57376ae50efae5dec5d1d053c
ibrag8998 <ibragdzh@gmail.com>
2020-07-09 20:58:39 +0300

    oops, developing on master

---
+++ snippets/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField()
+    linenos = serializers.BooleanField()
+    language = serializers.CharField()
+    style = serializers.CharField()
